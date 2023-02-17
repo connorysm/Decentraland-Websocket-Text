d1045a2742714a682b78256fa804df342c87641a
Connor YS Matla <connor.matla0@gmail.com>
2023-02-17 14:49:35 -0500

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import * as WebSocket from "ws";
 //twitter set up
 const needle = require('needle');
-require('dotenv/config');
-// Access BEARER_TOKEN from process.env object
-const BEARER_TOKEN = process.env.BEARER_TOKEN;
+
+//import dotenv from 'dotenv';
+//require('dotenv/config');
+// Access BEARER_TOKEN from .env
+//const BEARER_TOKEN = process.env.BEARER_TOKEN;
+const BEARER_TOKEN = 'BEARER TOKEN HERE'
 
 
 const endpointURL = "https://api.twitter.com/2/tweets/search/recent";
+
+interface Tweet {
+  text: string;
+}
 
 async function getTweets() {
 
@@ -14,9 +21,9 @@ async function getTweets() {
   // specify Tweet IDs to fetch, and any additional fields that are required
   // by default, only the Tweet ID and text are returned
   const params = {
-      "query": "DCL",
-      "tweet.fields": "lang,author_id",
-      "user.fields": "created_at"
+      "query": "DragonCity",
+      "tweet.fields": "lang,text",
+      //"user.fields": "created_at",
   }
 
   // this is the HTTP header that adds bearer token authentication
@@ -26,13 +33,23 @@ async function getTweets() {
           "authorization": `Bearer ${BEARER_TOKEN}`
          
       }
-  })
+  });
 
   if (res.body) {
-      return res.body;
+      const tweets: Tweet[] = Array.from(res.body.data)
+        .filter((tweet: any) => !tweet.text.match(/[#@]/))
+        .map((tweet: any) => {
+          return {
+            text: tweet.text,
+          };
+        });
+    return tweets;
+     
   } else {
       throw new Error('Unsuccessful request');
   }
+
+  
 }
 
 
@@ -46,6 +63,10 @@ interface customWs extends WebSocket {
 
 let tweet:any
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 wss.on("connection", async (clientWs, request) => {
@@ -53,14 +74,24 @@ wss.on("connection", async (clientWs, request) => {
   ws.room = request.url || "";
   
 
+
+///////////////////////////////////////////
+
   wss.clients.forEach(async function each(client) {
     const cWs = client as customWs;
+    const tweets: Tweet[] = await getTweets();
 
-    
+    const randomIndex = Math.floor(Math.random() * tweets.length);
+    const randomTweet = tweets[randomIndex];
+
+
+    const tweetText: string = randomTweet.text;
+
       tweet = await getTweets();
-      console.log(tweet)
-      tweet = JSON.stringify(tweet)
-      cWs.send(tweet);
+      //tweet = JSON.stringify(tweet);
+      //console.log(tweet)
+      console.log(tweetText)
+      cWs.send(tweetText);
       
       
       
